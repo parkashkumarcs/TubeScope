be9a2820d2507e86daf9b627f5583481617ac076
parkashkumarcs <parkashkumar.bscs.official@gmail.com>
2025-08-21 16:59:46 +0500

    App.jsx component codes added and updated1 on 08/21/2025

---
+++ tube-scope/src/App.jsx
@@ -0,0 +1,19 @@
+// App.jsx
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import ChannelInput from "./components/ChannelInput";
+import ChannelPage from "./components/ChannelPage";
+import "./App.css";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<ChannelInput />} />
+      <Route path="/channel/:id" element={<ChannelPage />} />
+      {/* Optional: redirect unmatched routes to home */}
+      <Route path="*" element={<ChannelInput />} />
+    </Routes>
+  );
+};
+
+export default App;
