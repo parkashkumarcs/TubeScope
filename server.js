1abdeaab23387b07e5446ea268295c34ce531198
parkashkumarcs <parkashkumar.bscs.official@gmail.com>
2025-08-26 23:51:54 +0500

    vercel.json added and updated33 on 08/26/2025

--- server.js
+++ server.js
@@ -134,7 +134,6 @@ app.get("/api/channel", async (req, res) => {
 });
 
 // 🚀 Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`✅ Server running at http://localhost:${PORT}`);
+app.listen(5000, () => {
+  console.log("✅ Server running at http://localhost:5000");
 });
